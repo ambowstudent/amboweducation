da77884a6ceb13b89393abd1cee03c7429e2efda
xiaobobo <994301552@qq.com>
2019-07-06 18:00:37 +0800

    mapper xml 修改 wb

--- ambowEducation/src/main/java/com/ambowEducation/dao/UserDao.java
+++ ambowEducation/src/main/java/com/ambowEducation/dao/UserDao.java
@@ -1,4 +1,13 @@
 package com.ambowEducation.dao;
 
-public class UserDao {
+
+import com.ambowEducation.po.User;
+import org.apache.ibatis.annotations.Insert;
+
+public interface UserDao {
+
+
+    @Insert("insert into t_user values(default,#{username},#{password},#{createtime})")
+    public int insertUser(User user);
+
 }
