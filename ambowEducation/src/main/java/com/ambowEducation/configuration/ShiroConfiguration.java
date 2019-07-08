3126928b5cd6590d92236e36ff1843629df46024
1359029494@qq.com <1359029494@qq.com>
2019-07-08 11:36:49 +0800

    yc 历史表sql提交

--- ambowEducation/src/main/java/com/ambowEducation/configuration/ShiroConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.ambowEducation.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-public class ShiroConfiguration {
-}
--- ambowEducation/src/main/java/com/ambowEducation/dao/HistoryMapper.java
+++ ambowEducation/src/main/java/com/ambowEducation/dao/HistoryMapper.java
@@ -1,5 +1,20 @@
 package com.ambowEducation.dao;
 
 
+import com.ambowEducation.po.History;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface HistoryMapper {
+    //增加历史
+    @Insert("insert into t_history values(default,#{tuId},#{tuName},#{sId},#{sName},#{reason},#{detail},#{editTime})")
+    public int insHistory(History h);
+
+    //查询全部也可模糊查询（学业导师看所有学生的历史）
+    public List<History> findAll(String key);
+
+    //学生看自己的历史（可以通过原因模糊查询）
+    public List<History> findMyHistory(String key, int sno);
 }
