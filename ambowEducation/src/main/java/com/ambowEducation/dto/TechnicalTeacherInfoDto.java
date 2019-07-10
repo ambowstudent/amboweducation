9c4028e1e00a1b46d0bfd19b0005cf4f9c6f7a1f
Smallbai <1140743786@qq.com>
2019-07-10 18:34:24 +0800

    TechnicalTeacher 和 Tutor Service  Bai

---
+++ ambowEducation/src/main/java/com/ambowEducation/dto/TechnicalTeacherInfoDto.java
@@ -0,0 +1,23 @@
+package com.ambowEducation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 这个类是用来封装管理员添加技术老师的dto
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TechnicalTeacherInfoDto {
+
+//    private Integer id; //技术教师ID
+    private String empNo;   //技术教师编号
+    private String name;    //技术教师姓名
+    private Integer sex;     //技术教师性别
+    private String skills;   //技术教师擅长的技术
+    private Integer workingSeniority;    //技术教师工作年限
+
+}
---
+++ ambowEducation/src/main/java/com/ambowEducation/dto/TutorInfoDto.java
@@ -0,0 +1,22 @@
+package com.ambowEducation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 这个类是用来封装管理员添加导师的dto
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TutorInfoDto {
+
+//    private Integer id; //ID
+    private String empNo;   //导师编号
+    private String name;    //导师姓名
+    private String sex;     //导师性别
+    private String time;    //导师工作年限
+
+}
