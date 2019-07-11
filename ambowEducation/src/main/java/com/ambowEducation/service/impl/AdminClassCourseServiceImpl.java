59ba520ec7a6460aae8dfa9498bbd3b80432cd28
Smallbai <1140743786@qq.com>
2019-07-11 14:24:40 +0800

    ReduceHoursMapper 修改 管理员信息管理 bai

---
+++ ambowEducation/src/main/java/com/ambowEducation/service/impl/AdminClassCourseServiceImpl.java
@@ -0,0 +1,176 @@
+package com.ambowEducation.service.impl;
+
+import com.ambowEducation.Exception.AdminClassCourseException;
+import com.ambowEducation.dao.*;
+import com.ambowEducation.dto.ClassCourseDto;
+import com.ambowEducation.dto.CourseDto;
+import com.ambowEducation.po.Classroom;
+import com.ambowEducation.po.Clazz;
+import com.ambowEducation.po.Course;
+import com.ambowEducation.service.AdminClassCourseService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AdminClassCourseServiceImpl implements AdminClassCourseService {
+
+    @Autowired
+    private ClassMapper classMapper;
+
+    @Autowired
+    private CourseMapper courseMapper;
+
+    @Autowired
+    private ClassCourseMapper classCourseMapper;
+
+    @Autowired
+    private ClassroomMapper classroomMapper;
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void insertClazz(ClassCourseDto classCourseDto) {
+        if(classCourseDto==null){
+            throw new AdminClassCourseException(-1,"对象不存在");
+        }
+        Clazz c1 = classMapper.selectByClassname(classCourseDto.getName());
+        Classroom cr = classroomMapper.selectByRoomNumber(classCourseDto.getRoomName());
+        if(c1!=null){
+            throw new AdminClassCourseException(-2,"此班名已被使用");
+        }
+        if(cr==null){
+            throw new AdminClassCourseException(-6,"该教室不存在");
+        }
+        Clazz c3 = classMapper.selectByRoomId(cr.getId());
+        if(c3!=null){
+            throw new AdminClassCourseException(-6,"该教室已被使用");
+        }
+        Clazz clazz = new Clazz();
+        List cour_list = Arrays.asList(classCourseDto.getCrId());
+        classCourseDto.setRoomId(cr.getId());
+        BeanUtils.copyProperties(classCourseDto,clazz);
+        int result_cla = classMapper.insertClazz(clazz);
+        Clazz c2 = classMapper.selectByClassname(clazz.getName());
+        int result_clacou = classCourseMapper.insertClassCourse(c2.getId(),cour_list);
+        if(result_cla<1||result_clacou<1){
+            throw new AdminClassCourseException(-3,"添加失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void updateClazz(ClassCourseDto classCourseDto) {
+        if(classCourseDto==null){
+            throw new AdminClassCourseException(-1,"对象不存在");
+        }
+//        查看 新班名 的班级信息，看看是否存在
+        Clazz c1 = classMapper.selectByClassname(classCourseDto.getName());
+//        查看 新教室 的教室信息，看看是否存在
+        Classroom cr = classroomMapper.selectByRoomNumber(classCourseDto.getRoomName());
+//        如果 新班级存在且不是 原来的班级 则不可改
+        if(c1!=null&&!c1.getId().equals(classCourseDto.getId())){
+            throw new AdminClassCourseException(-2,"此班名已被使用");
+        }
+        if(cr==null){
+            throw new AdminClassCourseException(-6,"该教室不存在");
+        }
+//        根据 教室 查班级 获取班级信息
+        Clazz c3 = classMapper.selectByRoomId(cr.getId());
+        //        如果 新班级存在且不是 原来的班级 则不可改
+        if(c3!=null&&!c3.getId().equals(classCourseDto.getId())){
+            throw new AdminClassCourseException(-6,"该教室已被使用");
+        }
+        Clazz clazz = new Clazz();
+//        获取教室Id并赋值
+        classCourseDto.setRoomId(cr.getId());
+        BeanUtils.copyProperties(classCourseDto,clazz);
+        int result_cla = classMapper.updateClazz(clazz);
+        if(result_cla<1){
+            throw new AdminClassCourseException(-4,"修改失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void deleteClazz(ClassCourseDto classCourseDto) {
+        if(classCourseDto==null){
+            throw new AdminClassCourseException(-1,"对象不存在");
+        }
+        Clazz clazz = new Clazz();
+        BeanUtils.copyProperties(classCourseDto,clazz);
+        int result1 = classMapper.deleteClazz(classCourseDto.getId());
+        int result2 = classCourseMapper.deleteClassCourse(classCourseDto.getId());
+        if(result1<1||result2<1){
+            throw new AdminClassCourseException(-5,"删除失败");
+        }
+    }
+
+    @Override
+    public List<Clazz> selectClazz() {
+        return classMapper.selectClazzList();
+    }
+
+    //
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void insertCourse(CourseDto courseDto) {
+        if (courseDto==null){
+            throw new AdminClassCourseException(-1,"对象不存在");
+        }
+        Course cou = courseMapper.selectByName(courseDto.getName());
+        if(cou!=null){
+            throw new AdminClassCourseException(-2,"该课程已存在");
+        }
+        Course course = new Course();
+        BeanUtils.copyProperties(courseDto,course);
+        int result = courseMapper.insert(course);
+        if (result<1){
+            throw new AdminClassCourseException(-3,"添加失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void updateCourse(CourseDto courseDto) {
+        if (courseDto==null){
+            throw new AdminClassCourseException(-1,"对象不存在");
+        }
+//        根据 新课程名 查询课程信息 ，查看是否存在
+        Course cou = courseMapper.selectByName(courseDto.getName());
+//        如果能查课程且此课程不是原来的课程，则不可修改
+        if(cou!=null&&!cou.getId().equals(courseDto.getId())){
+            throw new AdminClassCourseException(-2,"该课程已存在");
+        }
+        Course course = new Course();
+        BeanUtils.copyProperties(courseDto,course);
+        int result = courseMapper.update(course);
+        if (result<1){
+            throw new AdminClassCourseException(-4,"修改失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void deleteCourse(CourseDto courseDto) {
+        if (courseDto==null){
+            throw new AdminClassCourseException(-1,"对象不存在");
+        }
+        Course course = new Course();
+        BeanUtils.copyProperties(courseDto,course);
+        int result = courseMapper.delete(course.getId());
+        if (result<1){
+            throw new AdminClassCourseException(-3,"删除失败");
+        }
+    }
+
+    @Override
+    public List<Course> selectCourse() {
+        return courseMapper.findAllCourse();
+    }
+}
---
+++ ambowEducation/src/main/java/com/ambowEducation/service/impl/AdminOtherServiceImpl.java
@@ -0,0 +1,145 @@
+package com.ambowEducation.service.impl;
+
+import com.ambowEducation.Exception.AdminOtherException;
+import com.ambowEducation.dao.ClassroomMapper;
+import com.ambowEducation.dao.ReduceHoursMapper;
+import com.ambowEducation.dto.ClassroomDto;
+import com.ambowEducation.dto.ReduceHoursDto;
+import com.ambowEducation.po.Classroom;
+import com.ambowEducation.po.ReduceHours;
+import com.ambowEducation.service.AdminOtherService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AdminOtherServiceImpl implements AdminOtherService {
+
+    @Autowired
+    private ReduceHoursMapper reduceHoursMapper;
+
+    @Autowired
+    private ClassroomMapper classroomMapper;
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void insertReduceHours(ReduceHoursDto reduceHoursDto) {
+        if(reduceHoursDto==null){
+            throw new AdminOtherException(-1,"对象不存在");
+        }
+        ReduceHours rh = reduceHoursMapper.selectByName(reduceHoursDto.getName());
+        if (rh!=null){
+            throw new AdminOtherException(-2,"此扣分项已存在");
+        }
+
+        ReduceHours reduceHours = new ReduceHours();
+        BeanUtils.copyProperties(reduceHoursDto,reduceHours);
+
+        int result = reduceHoursMapper.insert(reduceHours);
+        if (result<1){
+            throw new AdminOtherException(-3,"添加失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void updateReduceHours(ReduceHoursDto reduceHoursDto) {
+        if(reduceHoursDto==null){
+            throw new AdminOtherException(-1,"对象不存在");
+        }
+        ReduceHours rh = reduceHoursMapper.selectByName(reduceHoursDto.getName());
+        if(rh!=null&&!rh.getId().equals(reduceHoursDto.getId())){
+            throw new AdminOtherException(-1,"此扣分项已存在");
+        }
+        ReduceHours reduceHours = new ReduceHours();
+        BeanUtils.copyProperties(reduceHoursDto,reduceHours);
+
+        int result = reduceHoursMapper.update(reduceHours);
+        if (result<1){
+            throw new AdminOtherException(-4,"修改失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void deleteReduceHours(ReduceHoursDto reduceHoursDto) {
+        if(reduceHoursDto==null){
+            throw new AdminOtherException(-1,"对象不存在");
+        }
+        ReduceHours reduceHours = new ReduceHours();
+        BeanUtils.copyProperties(reduceHoursDto,reduceHours);
+
+        int result = reduceHoursMapper.delete(reduceHours.getId());
+        if (result<1){
+            throw new AdminOtherException(-3,"删除失败");
+        }
+    }
+
+    @Override
+    public List<ReduceHours> selectReduceHours() {
+
+        return reduceHoursMapper.selectList();
+    }
+
+    //
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void insertClassroom(ClassroomDto classroomDto) {
+        if (classroomDto==null){
+            throw new AdminOtherException(-1,"对象不存在");
+        }
+        Classroom cr = classroomMapper.selectByRoomNumber(classroomDto.getRoomNumber());
+        if(cr!=null){
+            throw new AdminOtherException(-2,"该房间号已存在");
+        }
+        Classroom classroom = new Classroom();
+        BeanUtils.copyProperties(classroomDto,classroom);
+        int result = classroomMapper.insert(classroom);
+        if(result<1){
+            throw new AdminOtherException(-3,"添加失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void updateClassroom(ClassroomDto classroomDto) {
+        if (classroomDto==null){
+            throw new AdminOtherException(-1,"对象不存在");
+        }
+        Classroom cr = classroomMapper.selectByRoomNumber(classroomDto.getRoomNumber());
+        if(cr!=null&&!cr.getId().equals(classroomDto.getId())){
+            throw new AdminOtherException(-2,"该房间号已存在");
+        }
+        Classroom classroom = new Classroom();
+        BeanUtils.copyProperties(classroomDto,classroom);
+        int result = classroomMapper.update(classroom);
+        if(result<1){
+            throw new AdminOtherException(-4,"修改失败");
+        }
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    public void deleteClassroom(ClassroomDto classroomDto) {
+        if (classroomDto==null){
+            throw new AdminOtherException(-1,"对象不存在");
+        }
+        Classroom classroom = new Classroom();
+        BeanUtils.copyProperties(classroomDto,classroom);
+        int result = classroomMapper.delete(classroom.getId());
+        if(result<1){
+            throw new AdminOtherException(-5,"删除失败");
+        }
+    }
+
+    @Override
+    public List<Classroom> selectClassroom() {
+
+        return classroomMapper.selectList();
+    }
+}
