8fac18feb9dc130581c2e1af9b4d8ce651cc9c05
Administrator <1029141025@qq.com>
2019-08-04 18:15:08 +0800

    get改成post

--- ambowEducation/src/main/java/com/ambowEducation/controller/TechnicalTeacherController.java
+++ ambowEducation/src/main/java/com/ambowEducation/controller/TechnicalTeacherController.java
@@ -56,7 +56,7 @@ public class TechnicalTeacherController {
     }
     //根据学号跟课程id查询学生信息以及分数
 
-    @GetMapping("get_grade_info")
+    @PostMapping("get_grade_info")
     public JsonData getGradeInfo(@RequestBody  StudentGradeDto studentGradeDto,@RequestParam(defaultValue = "1",value = "pageNum") Integer pageNum){
         try {
             PageHelper.startPage(pageNum, PageUtil.PAGE_SIZE_GRADE);
